589ebaa227f483d47cefa52812aeee228acbeb31
jjcross <jjcross@umich.edu>
2016-05-02 23:22:59 -0400

    Added click back in and flexbox grid styling

--- app/App.jsx
+++ app/App.jsx
@@ -2,11 +2,42 @@ import React from 'react';
 import Background from './Background.jsx';
 
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			backgroundHue: Math.floor(Math.random() * 360)
+		};
+	};
 	render() {
 		return (
-			<div>
-				<Background />
+			<div className="container" onClick={this.containerClick}>
+				<Background hue={this.state.backgroundHue} />
+
+				<div className="layout">
+					<div className="left-col">
+						<div className="top-row"></div>
+						<div className="middle-row"></div>
+						<div className="bottom-row"></div>
+					</div>
+
+					<div className="center-col">
+						<div className="top-row"></div>
+						<div className="middle-row"></div>
+						<div className="bottom-row"></div>
+					</div>
+
+					<div className="right-col">
+						<div className="top-row"></div>
+						<div className="middle-row"></div>
+						<div className="bottom-row"></div>
+					</div>
+				</div>
 			</div>
 		);
+	};
+	containerClick = () => {
+		const backgroundHue = Math.floor(Math.random() * 360);
+		this.setState({backgroundHue});
 	};
 } 
--- app/Background.jsx
+++ app/Background.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 
 export default class Background extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			randHue: Math.floor(Math.random() * 360)
-		};
-	};
 	render() {
-		const colorString = `hsla(${this.state.randHue}, 100%, 50%, 1), hsla(${this.state.randHue + 75}, 100%, 50%, 1)`;
+		const {hue} = this.props;
+
+		const colorString = `hsla(${hue}, 100%, 50%, 1), hsla(${hue + 75}, 100%, 50%, 1)`;
 
 		const backgroundStyle = {
-			background: `hsla(${this.state.randHue}, 100%, 50%, 1)`,
+			background: `hsla(${hue}, 100%, 50%, 1)`,
 			background: `-webkit-linear-gradient(left top, ${colorString})`,
 			background: `-o-linear-gradient(bottom right, ${colorString})`,
 			background: `-moz-linear-gradient(bottom right, ${colorString})`,
@@ -20,11 +15,8 @@ export default class Background extends React.Component {
 		};
 
 		return (
-			<div style={backgroundStyle} className="background" onClick={this.backgroundClick}></div>
+			<div style={backgroundStyle} className="background" onClick={this.backgroundClick}>
+			</div>
 		);
-	};
-	backgroundClick = () => {
-		let randHue = Math.floor(Math.random() * 360);
-		this.setState({randHue});
 	};
 } 
