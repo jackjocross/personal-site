4294dd60faa4bb9356f39d2c509e08089c90a1c6
jcros8 <jjcross@umich.edu>
2016-05-15 23:47:14 -0400

    layout border styling

--- app/Layout.jsx
+++ app/Layout.jsx
@@ -2,53 +2,71 @@ import React from 'react';
 import PageSettings from './PageSettings.jsx';
 import styles from './layout.css';
 
+export default class Panel {
+	constructor(height, width, xPlacement, yPlacement, horizontalUnit, verticalUnit, columns, rows, target) {
+
+		this.panelClass = styles.panel;
+		if (xPlacement === 0) {
+			this.panelClass += ' ' + styles.clearBorderLeft;
+		} else if (xPlacement === columns - 1) {
+			this.panelClass += ' ' + styles.clearBorderRight;
+		} 
+
+		if (yPlacement === 0) {
+			this.panelClass += ' ' + styles.clearBorderTop;
+		} else if (yPlacement === rows - 1) {
+			this.panelClass += ' ' + styles.clearBorderBottom;
+		}
+
+		if (target) {
+			this.panelClass += ' ' + styles.clearBorder;
+		}
+
+		this.style = {
+			height: `${height.toFixed(2).toString()}%`,
+			width: `${width.toFixed(2).toString()}%`,
+			left: `${(xPlacement * horizontalUnit).toFixed(2).toString()}%`,
+			top: `${(yPlacement * verticalUnit).toFixed(2).toString()}%`
+		};
+	}
+}
+
 export default class Layout extends React.Component {
 	constructor(props) {
 		super(props);
 	
-		this.columns = 2;
-		this.rows = 2;
+		this.columns = 4;
+		this.rows = 4;
 
-		let verticalUnit = 100 / this.rows;
-		let horizontalUnit = 100 / this.columns;
-
-		this.createPanel = function Panel(heightIn, widthIn, xPlacementIn, yPlacementIn) {
-			
-			this.setStyle = (height, width, xPlacement, yPlacement) => {
-				this.style = {
-					height: `${height.toFixed(2).toString()}%`,
-					width: `${width.toFixed(2).toString()}%`,
-					left: `${(xPlacement * horizontalUnit).toFixed(2).toString()}%`,
-					top: `${(yPlacement * verticalUnit).toFixed(2).toString()}%`
-				};
-			};
-
-			this.setStyle(heightIn, widthIn, xPlacementIn, yPlacementIn);
-		};
+		this. verticalUnit = 100 / this.rows;
+		this. horizontalUnit = 100 / this.columns;
 
 		this.state = {
 			layoutGrid: []
 		};
 
 		this.initialGrid = [];
-
+		
+		// Create initialGrid and layoutGrid of panels
 		for (let i = 0;i < this.columns;i++) {
 			this.state.layoutGrid.push([]);
 			this.initialGrid.push([]);
 			for (let j = 0;j < this.rows;j++) {
-				this.state.layoutGrid[i].push(new this.createPanel(verticalUnit, horizontalUnit, i, j));
-				this.initialGrid[i].push(new this.createPanel(verticalUnit, horizontalUnit, i, j));
+				this.state.layoutGrid[i].push(new Panel(this.verticalUnit, this.horizontalUnit, i, j, this.horizontalUnit, this.verticalUnit, this.columns, this.rows));
+				this.initialGrid[i].push(new Panel(this.verticalUnit, this.horizontalUnit, i, j, this.horizontalUnit, this.verticalUnit, this.columns, this.rows));
 			}
 		}
 	}
 	render() {
 		return (
 			<div>
-				<button type="button" onClick={this.close}></button>
+				<div className={styles.header}>
+					<button type="button" onClick={this.close}>Back</button>
+				</div>
 				<div className={styles.layout}>
 					{this.state.layoutGrid.map((column, columnIndex) => {
 						return column.map((panel, rowIndex) => {
-							return <div style={this.state.layoutGrid[columnIndex][rowIndex].style} className={styles.panel} onClick={() => {this.panelClick(panel, columnIndex, rowIndex)}}></div>;
+							return <div style={this.state.layoutGrid[columnIndex][rowIndex].style} className={this.state.layoutGrid[columnIndex][rowIndex].panelClass} onClick={() => {this.panelClick(panel, columnIndex, rowIndex)}}></div>;
 						})
 					})}	
 				</div>
@@ -63,8 +81,10 @@ export default class Layout extends React.Component {
 			column.map((panel, rowIndex) => {
 				let colTargetDist = columnIndex - targetColumn;
 				let rowTargetDist = rowIndex - targetRow;
-				
-				layoutGrid[columnIndex][rowIndex] = new this.createPanel(100, 100, (colTargetDist * this.columns), (rowTargetDist * this.rows));
+
+				let isTarget = targetColumn === columnIndex && targetRow === rowIndex ? true : false;
+
+				layoutGrid[columnIndex][rowIndex] = new Panel(100, 100, (colTargetDist * this.columns), (rowTargetDist * this.rows), this.horizontalUnit, this.verticalUnit, this.columns, this.rows, isTarget);
 			});
 		});
 
@@ -75,7 +95,7 @@ export default class Layout extends React.Component {
 
 		layoutGrid.map((column, columnIndex) => {
 			column.map((panel, rowIndex) => {
-				layoutGrid[columnIndex][rowIndex] = Object.assign({}, this.initialGrid[columnIndex][rowIndex]);
+				layoutGrid[columnIndex][rowIndex] = Object.create(this.initialGrid[columnIndex][rowIndex]);
 			});
 		});
 
