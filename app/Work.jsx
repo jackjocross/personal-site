ecec24dfd903373edeb8f954c47e1ef196ffe36c
jjcross <jjcross@umich.edu>
2016-05-24 23:23:02 -0400

    Started separate section components

---
+++ app/Work.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PanelIcon from './PanelIcon.jsx';
+import styles from './about.css';
+
+export default class Work extends React.Component {
+	render() {
+		return (
+			<div className={styles.scrollableContainer}>
+				<div className={styles.contentContainer}>
+					<h1>
+						Work
+					</h1>
+					<p>
+						Creativity is a fucking work-ethic. Make your work consistent but not fucking predictable. If you fucking give up, you will achieve nothing. Think about all the fucking possibilities. Saul Bass on failure: Failure is built into creativity… the creative act involves this element of ‘newness’ and ‘experimentalism,’ then one must expect and accept the fucking possibility of failure. You are not your fucking work. Respect your fucking craft. You are not your fucking work.
+					</p>
+					<p>
+						Learn from fucking criticism. Saul Bass on failure: Failure is built into creativity… the creative act involves this element of ‘newness’ and ‘experimentalism,’ then one must expect and accept the fucking possibility of failure. Paul Rand once said, “The public is more familiar with bad fucking design than good design. It is, in effect, conditioned to prefer bad design, because that is what it lives with. The new becomes threatening, the old reassuring.” Fuck. Must-do is a good fucking master.
+					</p>
+					<p>
+						Remember it’s called the creative process, it’s not the creative fucking moment. Must-do is a good fucking master. When you design, you have to draw on your own fucking life experiences. If it’s not something you would want to read/look at/use then why fucking bother? Use your fucking hands. Sometimes it is appropriate to place various typographic elements on the outside of the fucking left margin of text to maintain a strong vertical axis. This practice is referred to as exdenting and is most often used with bullets and quotations. Must-do is a good fucking master.
+					</p>
+				</div>
+			</div>
+		);
+	}
+}
