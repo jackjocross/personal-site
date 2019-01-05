2b792e8e238959de2d455b63061c68ae1cb7eaa9
jack <jackjocross@gmail.com>
2019-01-05 18:09:42 -0500

    Add favicons

--- static.config.js
+++ static.config.js
@@ -22,6 +22,26 @@ export default {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <link
           href="https://unpkg.com/firacode@1.205.0/distr/fira_code.css"
           rel="stylesheet"
         />
